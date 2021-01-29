31f332b193541cb69fd395917e44f8c37a09788e
Arthur Taylor <arthur@codders.de>
2021-01-29 17:08:09 +0100

    Updates with classes

---
+++ VanBaby/GPSCoordinate.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace VanBaby;
+
+class GPSCoordinate {
+  private float $latitude;
+  private float $longitude;
+  private float $altitude;
+
+  public function toString(): string
+  {
+    return $this->getLatitude() . "," . $this->getLongitude();
+  }
+
+  private static function hex2float(string $hex): float
+  {
+    $dec = hexdec($hex);
+
+    if ($dec === 0) {
+        return 0;
+    }
+
+    $sup = 1 << 23;
+    $x = ($dec & ($sup - 1)) + $sup * ($dec >> 31 | 1);
+    $exp = ($dec >> 23 & 0xFF) - 127;
+    $sign = ($dec & 0x80000000) ? -1 : 1;
+
+    return $sign * $x * pow(2, $exp - 23);
+  }
+
+  public function setLatitude(float $latitude): self
+  {
+    $this->latitude = $latitude;
+    return $this;
+  }
+
+  public function getLatitude(): float
+  {
+    return $this->latitude;
+  }
+
+  public function setLongitude(float $longitude): self
+  {
+    $this->longitude = $longitude;
+    return $this;
+  }
+
+  public function getLongitude(): float
+  {
+    return $this->longitude;
+  }
+
+  public function setAltitude(float $altitude): self
+  {
+    $this->altitude = $altitude;
+    return $this;
+  }
+
+  public function getAltitude(): float
+  {
+    return $this->altitude;
+  }
+
+  public static function parseHexFloatTriple(string $hexString): ?GPSCoordinate
+  {
+    if (strlen($hexString) !== 24) {
+      trigger_error("GPS String should be 24 characters - got " . strlen($hexString), E_USER_NOTICE);
+      return null;
+    }
+    $latitude = substr($hexString, 0, 8);
+    $longitude = substr($hexString, 8, 8);
+    $altitude = substr($hexString, 16, 8);
+    return (new GPSCoordinate())
+      ->setLongitude(self::hex2float($longitude))
+      ->setLatitude(self::hex2float($latitude))
+      ->setAltitude(self::hex2float($altitude));
+  }
+}
--- echo.php
+++ echo.php
@@ -1,11 +1,43 @@
 <?php
-$time = gmdate("Ymd\TH:i:s\Z", $_GET["time"]);
-$hex_data = hex2bin($_GET["data"]);
-$data = $_GET["data"];
-if (strpos("No Signal", $hex_data) === 0) {
-	$data = "No Signal";
+declare(strict_types=1);
+
+include("VanBaby/GPSCoordinate.php");
+use VanBaby\GPSCoordinate;
+
+const TIME = "time";
+const DATA = "data";
+
+if (!array_key_exists(TIME, $_GET) || !array_key_exists(DATA, $_GET)) {
+  echo("Not enough data - supply time and data" . PHP_EOL);
+  exit(1);
 }
-$line = $time . "|" . $data;
+
+function getTime(): string
+{
+  $timeval = $_GET[TIME];
+  if (!is_numeric($timeval)) {
+    echo("Time must be an int" . PHP_EOL);
+    exit(1);
+  }
+  return gmdate("Ymd\TH:i:s\Z", (int)$timeval);
+}
+
+function getData(): ?GPSCoordinate
+{
+  $datastring = $_GET[DATA];
+  if ("No Signal" === $datastring) {
+    return null;
+  }
+  if (strlen($datastring) % 2 !== 0) {
+    echo("Invalid data string '" . $datastring . "' - must be even length");
+    exit(1);
+  }
+  return GPSCoordinate::parseHexFloatTriple($datastring);
+}
+
+$time = getTime();
+$data = getData();
+$line = $time . "|" . (is_null($data) ? "No Signal" : $data->toString());
 $res = file_put_contents("script.log", $line . "\n", FILE_APPEND);
 print($line . "\n");
 ?>
